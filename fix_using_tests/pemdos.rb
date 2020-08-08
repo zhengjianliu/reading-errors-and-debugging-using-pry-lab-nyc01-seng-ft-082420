3b12482f340abb276096a88e0b6e11dd0d46bf77
ZHENGJIAN LIU <comezheng@gmail.com>
2020-08-08 22:38:56 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,10 @@
 # don't forget to add: require 'pry'
-
+require 'pry'
 def snake_it_up(string)
   if string[0] == "s"
-    10 * "s" + string
+    return 's' * 10 + string
   else
     string
   end
+  
 end
--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,15 +1,17 @@
 # don't forget to add: require 'pry'
 
+require 'pry'
+
 def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
 
 
 def state_log(star_date)
-  "Captain's Log, star date #{star_date}."
+  return "Captain's Log, star date #{star_date}."
 end
 
 def engage
-  puts state_log(date)
   date = generate_star_date
+  puts state_log(date)
 end
