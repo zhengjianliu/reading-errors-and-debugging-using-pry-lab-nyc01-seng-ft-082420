424ad5d8fe5744c598bfceae95affb6454ef5348
ZHENGJIAN LIU <comezheng@gmail.com>
2020-08-08 22:38:46 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -8,10 +8,10 @@ end
 
 
 def state_log(star_date)
-  "Captain's Log, star date #{star_date}."
+  return "Captain's Log, star date #{star_date}."
 end
 
 def engage
-  puts state_log(date)
   date = generate_star_date
+  puts state_log(date)
 end
